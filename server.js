00a30f213367064a8883daea6a2f72891f927c25
Yuval Kashtan <yuvalkashtan@gmail.com>
2015-12-05 22:03:35 +0000

    add dynamic coverage map. it is still based directly on S3 need to convert to ddb or something else

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-var util = require('util'),
+var fs = require('fs'),
+    util = require('util'),
     http = require('http'),
     async = require('async'),
     AWS = require('aws-sdk'),
     uuid = require('node-uuid'),
     express = require('express'),
     bodyParser = require('body-parser'),
-    serveStatic = require('serve-static')
+    serveStatic = require('serve-static'),
     port = 1337;
 
 AWS.config.update({region: 'us-west-2'});
@@ -13,6 +14,19 @@ var s3 = new AWS.S3();
 var app = express();
 app.use(serveStatic(__dirname));
 app.use(bodyParser.json());
+
+app.get("/watchbat/737638bc-312c-495c-a5e4-113b90432ad4/logins", function(req,res) {
+	fs.readFile('logins.txt', function(error, content) {
+		if (error) {
+			res.writeHead(500);
+			res.end();
+		}
+		else {
+			res.writeHead(200, { 'Content-Type': 'text/plain' });
+			res.end(content, 'utf-8');
+		}
+	});
+});
 
 app.get("/watchbat/covermap", function(req,res) {
     console.log("!!!!");
@@ -22,30 +36,29 @@ var html = (function () {/*
 <!DOCTYPE html>
 <html>
   <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-    <title>Heatmaps</title>
+    <title>Circles</title>
     <style>
-      html, body, #map-canvas {
+      html, body {
         height: 100%;
-        margin: 0px;
-        padding: 0px
+        margin: 0;
+        padding: 0;
       }
-      #panel {
-        position: absolute;
-        top: 5px;
-        left: 50%;
-        margin-left: -180px;
-        z-index: 5;
-        background-color: #fff;
-        padding: 5px;
-        border: 1px solid #999;
+      #map {
+        height: 100%;
       }
     </style>
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=visualization"></script>
+  </head>
+  <body>
+    <div id="map"></div>
     <script>
-var map, pointarray, heatmap;
 
-var taxiData = [
+// This example creates circles on the map, representing populations in North
+// America.
+
+// First, create an object containing LatLng and population for each city.
+var sessionsmap = {
 */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
 
     var params = {
@@ -71,10 +84,11 @@ var taxiData = [
                         try {
                             var occ = JSON.parse(content.Body.toString());
                             if (occ.action == "session" && occ.location) {
-                                var newloc = "new google.maps.LatLng(" + occ.location.start.location.latitude + ", " + occ.location.start.location.longitude + "),";
+				occ.id = occ.id.replace(/\-/g,'');
+				var newloc = occ.id + ": { center: { lat: " + occ.location.start.location.latitude + ", lng: " + occ.location.start.location.longitude + "}, mag: 1 }, ";
                                 console.log(newloc);
                                 html += newloc;
-                                newloc = "new google.maps.LatLng(" + occ.location.end.location.latitude + ", " + occ.location.end.location.longitude + "),";
+				var newloc = occ.id + ": { center: { lat: " + occ.location.end.location.latitude + ", lng: " + occ.location.end.location.longitude + "}, mag: 1 }, ";
                                 console.log(newloc);
                                 html += newloc;
                             }
@@ -86,82 +100,55 @@ var taxiData = [
                 });
             }, function (err) {
 html += (function () {/*
-];
+};
 
-function initialize() {
-  var mapOptions = {
+function initMap() {
+  // Create the map.
+  var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 9,
     center: new google.maps.LatLng(31.631893,34.6761535),
     mapTypeId: google.maps.MapTypeId.SATELLITE
-  };
-
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-  var pointArray = new google.maps.MVCArray(taxiData);
-
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: pointArray
   });
 
-  heatmap.setMap(map);
+  // Construct the circle for each value in citymap.
+  // Note: We scale the area of the circle based on the population.
+  for (var sess in sessionsmap) {
+    // Add the circle for this city to the map.
+    var sessCircle = new google.maps.Circle({
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: sessionsmap[sess].center,
+      radius: Math.sqrt(sessionsmap[sess].mag) * 1000
+    });
+  }
 }
-
-function toggleHeatmap() {
-  heatmap.setMap(heatmap.getMap() ? null : map);
-}
-
-function changeGradient() {
-  var gradient = [
-    'rgba(0, 255, 255, 0)',
-    'rgba(0, 255, 255, 1)',
-    'rgba(0, 191, 255, 1)',
-    'rgba(0, 127, 255, 1)',
-    'rgba(0, 63, 255, 1)',
-    'rgba(0, 0, 255, 1)',
-    'rgba(0, 0, 223, 1)',
-    'rgba(0, 0, 191, 1)',
-    'rgba(0, 0, 159, 1)',
-    'rgba(0, 0, 127, 1)',
-    'rgba(63, 0, 91, 1)',
-    'rgba(127, 0, 63, 1)',
-    'rgba(191, 0, 31, 1)',
-    'rgba(255, 0, 0, 1)'
-  ]
-  heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
-}
-
-function changeRadius() {
-  heatmap.set('radius', heatmap.get('radius') ? null : 20);
-}
-
-function changeOpacity() {
-  heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
 
     </script>
-  </head>
-
-  <body>
-    <div id="panel">
-      <button onclick="toggleHeatmap()">Toggle Heatmap</button>
-      <button onclick="changeGradient()">Change gradient</button>
-      <button onclick="changeRadius()">Change radius</button>
-      <button onclick="changeOpacity()">Change opacity</button>
-    </div>
-    <div id="map-canvas"></div>
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCj_jIOhRtmxMq-X4lDB4xjExeZorlKPlc&signed_in=true&callback=initMap"></script>
   </body>
 </html>
 */}).toString().match(/[^]*\/\*([^]*)\*\/\}$/)[1];
+
     res.end(html);
             });
         }
     });
 
 });
-
+app.post("/watchbat/api/login", function(req,res) {
+	console.log("login: " + JSON.stringify(req.body));
+	if (req.body.observer("eyalvanunu@gmail.com") > -1) {
+		// User is Eyal, not interesting
+	} else {
+		// not eyal
+        fs.appendFile('logins.txt', '\n' + req.body.logintime + ' ' + req.body.observer.displayName + ' emails:' + JSON.stringify(req.body.observer.emails), function (err) {});
+	}
+});
 app.get("/watchbat/heatmap", function(req,res) {
     console.log("!!!!");
     res.writeHead(200, {"Content-Type": "text/html"});
@@ -308,6 +295,12 @@ google.maps.event.addDomListener(window, 'load', initialize);
 });
 app.post("/watchbat/api/login", function(req,res) {
 	console.log("login: " + JSON.stringify(req.body));
+	if (req.body.observer("eyalvanunu@gmail.com") > -1) {
+		// User is Eyal, not interesting
+	} else {
+		// not eyal
+        fs.appendFile('logins.txt', '\n' + req.body.logintime + ' ' + req.body.observer.displayName + ' emails:' + JSON.stringify(req.body.observer.emails), function (err) {});
+	}
 });
 app.post("/watchbat/api", function(req,res) {
     var facts=observation2facts(req.body);
